f4c31c28dc392c157aa7755688484f5b1cc56be4
kristian <kbouwser@gmail.com>
2016-12-13 22:37:01 -0500

    add and configure devise for User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_many :user_games, dependent: :destroy
   has_many :games, through: :user_games
