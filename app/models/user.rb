2155607711006725c4c731bd3180cf2c4b214fc9
Andrei Kislichenko <android.2net@gmail.com>
2017-10-31 22:14:26 +0100

    end subscription and delete customer when delete user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_destroy :unsubscribe
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -15,6 +16,19 @@ class User < ApplicationRecord
       ug.updated_at
     else
       updated_at
+    end
+  end
+
+  private
+
+  def unsubscribe
+    Stripe.api_key = ENV["STRIPE_API_KEY"]
+    if subscription_id && period_end && period_end > DateTime.now
+      sub = Stripe::Subscription.retrieve(subscription_id)
+      sub.delete
+    end
+    if stripe_id
+      Stripe::Customer.retrieve(stripe_id).delete
     end
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,42 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @user = users :two
+  end
+
+  test "should unsubscribe and delete customer before delete" do
+    @user.update period_end: (DateTime.now + 1.day), subscription_id: "sub_BbW7HRjhVh0EpK"
+    subscription_mock, subscription_stub = stripe_subscription_retrive_mock
+    customer_mock, customer_stub = stripe_customer_retrieve_mock
+    Stripe::Customer.stub :retrieve, customer_stub do
+      Stripe::Subscription.stub :retrieve, subscription_stub do
+        @user.destroy
+      end
+    end
+    assert_mock customer_mock
+    assert_mock subscription_mock
+  end
+
+  private
+
+  def stripe_subscription_retrive_mock
+    subscription = Minitest::Mock.new
+    subscription.expect :delete, {}
+    retrieve_stub = lambda do |subscription_id|
+      assert subscription_id.is_a? String
+      subscription
+    end
+    [subscription, retrieve_stub]
+  end
+
+  def stripe_customer_retrieve_mock
+    customer = Minitest::Mock.new
+    customer.expect :delete, {}
+    retrieve_stub = lambda do |customer_id|
+      assert customer_id.is_a? String
+      customer
+    end
+    [customer, retrieve_stub]
+  end
 end
