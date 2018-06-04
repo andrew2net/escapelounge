1ae32f4cf3b45c755b11fe2c689b9e954ede903b
Andrei Kislichenko <android.2net@gmail.com>
2018-06-04 13:42:29 +0200

    redirect to games_path after reset password

--- app/controllers/user/passwords_controller.rb
+++ app/controllers/user/passwords_controller.rb
@@ -1,6 +1,14 @@
-class PasswordsController < Devise::PasswordsController
+# frozen_string_literal: true
+
+# Uesr password controooler
+class User::PasswordsController < Devise::PasswordsController
   protected
-  def after_sending_reset_password_instructions_path_for(resource_name)
+
+  def after_sending_reset_password_instructions_path_for(_resource_name)
+    games_path
+  end
+
+  def after_resetting_password_path_for(_resource_name)
     games_path
   end
 end
